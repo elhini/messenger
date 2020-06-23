f52b4cb6e9e9f5a5f38c1e8dca1d1d3a661863ec
Artem Guslov <elhini@yandex-team.ru>
2020-06-24 00:34:24 +0300

    socket.io server

--- src/server.js
+++ src/server.js
@@ -4,14 +4,28 @@ const bodyParser     = require('body-parser');
 const cors           = require('cors');
 const dbConfig       = require('./config/db');
 const app            = express();
-const port = 8000;
+const port           = 8000;
+
 app.use(bodyParser.json());
 app.use(cors({ origin: '*' }));
+
 MongoClient.connect(dbConfig.url, {useUnifiedTopology: true}, (err, dbClient) => {
   if (err) return console.log(err);
   var db = dbClient && dbClient.db();
   require('./api')(app, db);
-  app.listen(port, () => {
+  var server = app.listen(port, () => {
     console.log('server listening on port ' + port);
-  });        
+  });
+
+  var io = require('socket.io').listen(server);
+  io.on('connection', (socket) => {
+    console.log('user connected');
+    socket.on('new-message', (msg) => {
+      console.log('new-message', msg);
+      io.emit('new-message', msg);
+    });
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
+  });
 });
