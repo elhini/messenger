682adaf5430d6ac9fc3bd5802352b4ef551b3593
Artem Guslov <elhini@yandex-team.ru>
2020-06-25 12:16:41 +0300

    show new chat form if no chat with user found

--- src/components/ChatList/ChatList.js
+++ src/components/ChatList/ChatList.js
@@ -3,10 +3,12 @@ import { connect } from 'react-redux';
 import { setChats, setActiveChat } from '../../actions';
 import { toStr } from '../../utils/date';
 import { req } from '../../utils/async';
+import NewChat from '../NewChat';
 import './ChatList.scss'; 
 
 function ChatList({ user, chats, setChats, activeChatID, setActiveChat }) {
     const [search, setSearch] = useState('');
+    const [status, setStatus] = useState('');
     const chatsByUser = chats.filter(c => c.users.includes(user));
     const chatsBySearch = search ? chatsByUser.filter(c => {
         var receiver = c.users.find(u => u !== user);
@@ -14,14 +16,13 @@ function ChatList({ user, chats, setChats, activeChatID, setActiveChat }) {
     }) : chatsByUser;
 
     useEffect(() => {
-        if (chats.length) {
-            return;
-        }
+        setStatus('loading');
         req('GET', 'chats', null, res => {
+            setStatus('');
             const sortedChats = res.sort((c1, c2) => c2.lastMessageDate > c1.lastMessageDate ? 1 : -1);
             setChats(sortedChats);
         });
-    });
+    }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
     useEffect(() => {
         var chatIDs = chatsByUser.map(c => c._id);
@@ -30,16 +31,19 @@ function ChatList({ user, chats, setChats, activeChatID, setActiveChat }) {
         }
     });
 
+    var isLoading = status === 'loading';
     return <div className="ChatList">
         <form className="chatSearchForm">
             <input type="text" className="chatSearchInput" value={search} onChange={e => setSearch(e.target.value)} placeholder="Search by receiver name" />
         </form>
+        {isLoading ? <p>Loading chats...</p> : (!search && !chatsBySearch.length ? <p>No chats found</p> : null)}
+        {search && !chatsBySearch.length && <NewChat search={search} />}
         <div className="chatsCont">
             <ul className="chats">
                 {chatsBySearch.map(c => 
-                    <li className={'chat ' + (c._id === activeChatID ? 'active' : '')} key={c._id} data-id={c._id} onClick={e => setActiveChat(c._id)}>
+                    <li className={'chat ' + (c._id === activeChatID ? 'active' : '')} key={c._id} onClick={e => setActiveChat(c._id)}>
                         <span className="user">{c.users.find(u => u !== user)}</span>{' '}
-                        <span className="date">[{toStr(c.lastMessageDate)}]</span>
+                        {c.lastMessageDate && <span className="date">[{toStr(c.lastMessageDate)}]</span>}
                         <br /> 
                         {c.lastMessageUser === user ? 'You: ' : ''}{c.lastMessageText}
                     </li>
