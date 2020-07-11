65f5390c6fad6eeead55a874b3112ab8c9eb07fd
Artem Guslov <elhini@yandex-team.ru>
2020-07-11 23:10:36 +0300

    add users/login api handler

--- src/api/users.js
+++ src/api/users.js
@@ -14,14 +14,37 @@ class UsersAPI extends APIBase {
             'post /register': async (req, res) => {
                 var users = await this.methods['get'](req, res, null, {login: req.body.login}, true);
                 if (users[0]) {
-                    return res.send({ 'error': 'user with provided login already exists' });
+                    return res.send({ 'error': 'user already exists' });
                 }
                 bcrypt.hash(req.body.password, saltRounds, async (err, hash) => {
                     if (err) return console.error(err);
                     req.body.password = hash;
+                    req.body.registrationDate = new Date();
                     var user = await this.methods['post'](req, res, null, true);
                     delete user.password;
                     return res.send(user);
+                });
+            },
+            'post /login': async (req, res) => {
+                var users = await this.methods['get'](req, res, null, {login: req.body.login}, true);
+                if (!users[0]) {
+                    return res.send({ 'error': 'user not exists' });
+                }
+                if (!users[0].password) {
+                    return res.send({ 'error': 'user does not have password' });
+                }
+                bcrypt.compare(req.body.password, users[0].password, async (err, match) => {
+                    if (err) return console.error(err);
+                    if (match) {
+                        req.body.loginDate = new Date();
+                        var user = req.body; // TODO: await sessionsAPI['post'](req, res, null, true);
+                        delete user.password;
+                        res.cookie('logged-as', user.login, { maxAge: 24 * 60 * 60 * 1000, httpOnly: true });
+                        return res.send(user);
+                    }
+                    else {
+                        res.send({ 'error': 'password is wrong' });
+                    }
                 });
             }
         };
