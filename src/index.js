1babda9f3acb9821c2656d27828a4de7775e7b8d
Artem Guslov <elhini@yandex-team.ru>
2020-07-21 11:13:08 +0300

    handle chat events on socket server

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { combineReducers, createStore, } from 'redux';
-import { alerts, users, user, chats, messages } from './reducers'
+import { alerts, users, user, chats, messages } from './reducers';
 import './index.css';
 import App from './App';
 
--- src/socket-server.js
+++ src/socket-server.js
@@ -5,14 +5,24 @@ module.exports = (server) => {
 
         socket.on('join-chats', (user, chatIDs) => {
             console.log('user', [user.login], 'joined chats', chatIDs);
+            socket.join(user.login);
             chatIDs.forEach(chatID => socket.join(chatID));
         });
 
-        var events = ['new-message', 'upd-message', 'del-message'];
-        events.forEach(event => {
+        var chatEvents = ['new-chat', 'upd-chat', 'del-chat'];
+        chatEvents.forEach(event => {
+            socket.on(event, (user, chat) => {
+                console.log(event, 'between users', chat.users);
+                var usersToNotify = chat.users.filter(u => u !== user.login);
+                usersToNotify.forEach(login => io.in(login).emit(event, user, chat));
+            });
+        });
+
+        var messageEvents = ['new-message', 'upd-message', 'del-message'];
+        messageEvents.forEach(event => {
             socket.on(event, (msg) => {
-            console.log(event, 'from user', [msg.user], 'with text', [msg.text], 'for chat', [msg.chatID]);
-            io.in(msg.chatID).emit(event, msg);
+                console.log(event, 'from user', [msg.user], 'with text', [msg.text], 'for chat', [msg.chatID]);
+                io.in(msg.chatID).emit(event, msg);
             });
         });
 
